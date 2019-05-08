32ad7a3dfed3000aa764698fbbac4cc468e9da85
erekee <43266120+erekee@users.noreply.github.com>
2019-05-08 10:11:19 +0600

    Update case6.js

--- case6.js
+++ case6.js
@@ -7,7 +7,7 @@ browser
 .click('#game_area_purchase > div.game_area_purchase_game_wrapper > div > div.game_purchase_action > div > div.btn_addtocart > a')
 
 .click('#btn_purchase_gift')
-.setValue('#input_username', 'tungpr12')
+.setValue('#input_username', 'Eraba')
 .setValue('#input_password', 'dasdasdada')
 .pause(1000)
 .end();
